a82a63fc7393cfcab6c18b12d820377f3e6c7c48
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-07-10 12:08:30 -0300

    move home component + styles search

--- app-clima/src/app/app.module.ts
+++ app-clima/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ListCardsComponent } from './components/list-cards/list-cards.component';
 
@@ -18,7 +20,9 @@ import { ListCardsComponent } from './components/list-cards/list-cards.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-clima/src/app/components/search/search.component.ts
+++ app-clima/src/app/components/search/search.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  form!: FormGroup;
+  @Output() public emitSearch: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.form = this.formBuilder.group({
+      cidade: [null, Validators.required],
+    });
+  }
+
+  search() {
+    this.emitSearch.emit(this.form.value);
+  }
 }
