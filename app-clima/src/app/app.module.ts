1b12f9289919e41a9c494065e2441e02ccdb5905
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-07-10 10:46:15 -0300

    componentes e arquivos iniciais

--- app-clima/src/app/app.module.ts
+++ app-clima/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    ListCardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app-clima/src/app/shared/Constantes.ts
@@ -0,0 +1,3 @@
+export class Constantes {
+  public static URL_BASE = 'https://api.openweathermap.org/data/2.5/weather?q='
+}
